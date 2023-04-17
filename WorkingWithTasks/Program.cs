38c9bd99faa4447880c9fd47239345a2eba6933c
danielchoike2 <89948222+danielchoike2@users.noreply.github.com>
2023-04-17 14:15:08 -0400

    Add project files.

---
+++ WorkingWithTasks/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace threadRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read, Set Go...");
+
+            int t1Location = 0;
+            int t2Location = 0;
+            int t3Location = 0;
+            int t4Location = 0;
+            int t5Location = 0;
+
+
+
+            //Creating Threads
+            Task t1 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Speedy Gonzales";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100)
+                        MoveIt(ref t1Location);
+                }
+
+            });
+            Task t2 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Road Runner";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t2Location < 100)
+                        MoveIt(ref t2Location);
+
+                }
+            });
+            Task t3 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Flash";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t3Location < 100)
+                        MoveIt(ref t3Location);
+
+                }
+            });
+            Task t4 = Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Dash";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t4Location < 100)
+                        MoveIt(ref t4Location);
+
+                }
+            });
+            Thread t5 = new Thread(delegate ()
+            {
+                Thread.CurrentThread.Name = "Crash";
+                for (int i = 0; i < 100; i++)
+                {
+                    if (t1Location < 100 && t5Location < 100)
+                        MoveIt(ref t5Location);
+
+                }
+            });
+            Task.WaitAny(t2);
+
+            // Priority Code
+            //t2.Priority = ThreadPriority.AboveNormal;
+            ////////////////////////////////////////////////
+
+            //Executing the methods
+           
+            //
+            Console.WriteLine("Race has ended");
+        }
+        static void MoveIt(ref int location)
+        {
+            location++;
+            Console.WriteLine($"{Thread.CurrentThread.Name} location={location}");
+            if (location >= 100)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} is the winner");
+                return;
+            }
+        }
+
+    }
+}
+
